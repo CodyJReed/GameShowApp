f8e39da18de72bb853d3a5c7d1264c1424899ff7
Cody Reed <cody2j2reed@gmail.com>
2018-05-22 00:08:57 -0400

    finalized project.

--- js/app.js
+++ js/app.js
@@ -6,15 +6,11 @@ const startButton = document.querySelector('.btn__reset');
 const ul = phrase.querySelector('ul');
 const keyrow = qwerty.querySelectorAll('.keyrow');
 const keyboard = qwerty.getElementsByTagName('button');
-const live = document.querySelector('img');
-const li = live.parentNode;
-const ol = li.parentNode;
+const live = document.querySelectorAll('img');
+const headline = document.querySelector('#overlay h2');
 
+//Event Trackers
 
-
-
-let letterFound = false;
-let correct   = 0;
 let incorrect = 0; //Missed
 
 //Start Game listener and Overlay toggle
@@ -27,7 +23,7 @@ startButton.addEventListener('click', () => {
 //Phrases Array
 
 const phrases = [
-  'bicycle', 'tricycle', 'unicycle', 'quadricycle', 'pentacycle'
+  'looks like rain', 'sneaky devil', 'take the bait', 'eye on the prize', 'sink or swim'
 ];
 
 //getRandomPhraseArray function
@@ -50,11 +46,13 @@ function addPhraseToDisplay(arr) {
   for ( let i = 0; i < arr.length; i += 1) {
     let li = document.createElement('li');
     li.innerHTML = arr[i];
-    if (li.text != ' ') {
+    if (li.textContent != ' ') {
       li.setAttribute('class', 'letter');
+    } else if (li.textContent == ' ' ){
+      li.setAttribute('class', 'space');
     }
 //Add li element to parentNode
-    ul.appendChild(li);
+ul.appendChild(li);
   }
 }
 
@@ -68,12 +66,12 @@ for (let i = 0; i < keyboard.length; i += 1) {
   keyboard[i].addEventListener('click', (e) => {
 
 //If Keyboard is clicked ID the key's letter
-    let keyLetter = keyboard[i].innerHTML;
+    let keyLetter = keyboard[i].textContent;
 
 //Compare keyLetter to lisLetter with function
     checkLetter(keyLetter);
     keyboard[i].disabled = true;
-
+    checkWin();
   });
 }
 
@@ -81,25 +79,63 @@ for (let i = 0; i < keyboard.length; i += 1) {
 
 function checkLetter(keyLetter) {
   let lis = document.querySelectorAll('.letter');
-
+   let letterFound = false;
   for (let i = 0; i < lis.length; i ++) {
 
-    let lisLetter = lis[i].innerHTML;
+    let lisLetter = lis[i].textContent;
 
-    letterFound = false;
-    if ( lisLetter == keyLetter ) {
-       letterFound = true;
-      lis[i].setAttribute('class', 'show');
+    if ( lisLetter === keyLetter ) {
+      letterFound = true;
+      lis[i].classList.add('show');
+      lis[i].style.transition = 'background 1.0s ease-in-out 0s';
 
-      // return letterFound;
+
+
     }
   }
-
-
-  if (letterFound == false) {
-
-    li.removeChild(live);
+  if (letterFound === false) {
     incorrect += 1;
   }
+  if (incorrect == 1) {
+    live[0].style.display = 'none';
+  } else if (incorrect == 2) {
+    live[1].style.display = 'none';
+  }else if (incorrect == 3) {
+    live[2].style.display = 'none';
+  } else if (incorrect == 4) {
+    live[3].style.display = 'none';
+  } else if (incorrect == 5) {
+    live[4].style.display = 'none';
+  }
+  return letterFound;
+}
 
+// checkWin function
+
+function checkWin() {
+    let li = document.querySelectorAll('.letter');
+    let show = document.querySelectorAll('.show');
+   if (li.length === show.length){
+   headline.textContent = 'Congrats...You Win!'
+   overlay.style.display = 'flex';
+   startButton.textContent = 'Play Again?';
+   headline.classList.add('win');
+   reset();
+
+ } else if (incorrect === 5) {
+      headline.textContent = 'So Sorry...You Lose!'
+      overlay.style.display = 'flex';
+      startButton.textContent = 'Try Again?';
+      headline.classList.add('lose');
+      reset();
+  }
+}
+
+//Reset Game
+
+function reset(start) {
+
+   startButton.addEventListener('click', () =>{
+     window.location = window.location
+  });
 }
